7219a804a328c39cfb17b6eb84377a9d856cf169
ducan <chuducan272003@gmail.com>
2025-05-06 09:46:13 +0700

    #24

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-console.log("Hello world");
-console.log("nodejs pro");
+const { log } = require("console");
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+app.get("/ducandev", (req, res) => {
+    res.send("Hello ducan");
+});
+
+app.listen(8080, () => {
+    console.log(`My app is running on port: ${PORT}`);
+});
